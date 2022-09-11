3a06575609d472001b2b1c54eb8206f0cefe4878
Zac Todd <zactodd0@gmail.com>
2022-09-12 11:52:18 +1200

    Update fill.py

--- auto_fill/fill.py
+++ auto_fill/fill.py
@@ -1,9 +1,30 @@
 from datetime import datetime, timedelta
 import requests
 from lxml import html
+import attr
 
 
 _UCSA_DENTAL_URL = 'https://ucsa.org.nz/support/dental/'
+_UCSA_DENTAL_FORM = 'https://ucsa.org.nz/support/dental/#ctl00_dentalform_section'
+
+
+_FORM_VALUES = {
+    'ctl00$dentalform$question_30$txtTextbox': 'first_name',
+    'ctl00$dentalform$question_31$txtTextbox': 'last_name',
+    'ctl00$dentalform$question_32$txtTextbox': 'student_id',
+    'ctl00$dentalform$question_34$radioList': 'is_undergrad', # 76 or 77
+    'ctl00$dentalform$question_35$radioList': 'is_domestic', # 78 or 79
+    'ctl00$dentalform$question_36$txtTextbox': 'email',
+    'ctl00$dentalform$question_37$txtTextbox:': 'email',
+    'ctl00$dentalform$question_38$txtTextbox': 'phone',
+    'ctl00$dentalform$question_39$radioList': 'clinic', # 83 or 84
+    'ctl00$dentalform$question_40$txtTextbox': 'reason',
+}
+
+_FORM_REQUIRED = {
+    'ctl00$dentalform$question_41$radioList': '86',  # 86
+    'ctl00$dentalform$btnSubmit': 'Finish'  # Finish
+}
 
 
 # Request headers
@@ -22,8 +43,37 @@ _HEADERS = {
 }
 
 
-def fill() -> bool:
-    return False
+@attr.s
+class Info:
+    first_name: str = attr.ib()
+    last_name: str = attr.ib()
+    student_id: str = attr.ib()
+    is_undergrad: int = attr.ib(converter=lambda x: 76 if bool(x) else 77)
+    is_domestic: int = attr.ib(converter=lambda x: 78 if bool(x) else 79)
+    email: str = attr.ib()
+    phone: str = attr.ib()
+    clinic: int = attr.ib(converter=lambda x: 83 if bool(x) else 84)
+    reason: str = attr.ib()
+
+
+def fill(data) -> bool:
+    info = Info(data)
+    post_json = _FORM_REQUIRED
+    post_json.update(form_view_state())
+    post_json.update({k: info.__getattribute__(v) for k, v in _FORM_VALUES.items()})
+    r = requests.post(headers=_HEADERS, url=_UCSA_DENTAL_FORM, json=post_json)
+    return True
+
+
+def form_view_state():
+    r = requests.get(headers=_HEADERS, url=_UCSA_DENTAL_URL)
+    tree = html.fromstring(r.content)
+    return {
+        '__EVENTTARGET' : '',
+        '__EVENTARGUMENT' : '',
+        '__VIEWSTATE': tree.xpath("//input[@id='__VIEWSTATE']")[0].value,
+        '__VIEWSTATEGENERATOR' : tree.xpath("//input[@id='__VIEWSTATEGENERATOR']")[0].value
+    }
 
 
 def form_exists() -> bool:
@@ -61,7 +111,7 @@ def block_till_form_exist(release, wait) -> bool:
     return has_form
 
 
-def fill_on_release(release, wait=timedelta(minutes=10)) -> bool:
+def fill_on_release(release, data, wait=timedelta(minutes=10)) -> bool:
     """
     Fill the form when it is released
     :param release: The time the form is released.
@@ -70,6 +120,6 @@ def fill_on_release(release, wait=timedelta(minutes=10)) -> bool:
     """
     block_til_release(release)
     if block_till_form_exist(release, wait):
-        return fill()
+        return fill(data)
     else:
         return False
